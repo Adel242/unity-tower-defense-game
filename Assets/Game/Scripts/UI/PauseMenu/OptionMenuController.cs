fa88f18eba9df7c9a8c1da610937fb7b95b0e9d1
Adel242 <brian.progra.91@gmail.com>
2026-08-30 19:43:45 -0400

    new_optiones_sonido

---
+++ Assets/Game/Scripts/UI/PauseMenu/OptionMenuController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class OptionsMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject soundPanel;
+
+    public void OpenOptions()
+    {
+        pausePanel.SetActive(false);
+        soundPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+        soundPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    public void OpenSound()
+    {
+        optionsPanel.SetActive(false);
+        soundPanel.SetActive(true);
+    }
+
+    public void CloseSound()
+    {
+        soundPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+}
